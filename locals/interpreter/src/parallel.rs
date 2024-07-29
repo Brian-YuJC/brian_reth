1454ef56f7478cc30db0031a0f7699d31c538436
Brian <brian.jincheng.yu@outlook.com>
2024-07-29 18:26:52 +0800

    [dev v0.0.18]Add struct Batch. Now a batch of blockmsg will be send to print thread after batch size equals split size(how many blocks in a file)

--- locals/interpreter/src/parallel.rs
+++ locals/interpreter/src/parallel.rs
@@ -13,7 +13,7 @@ pub static OP_CHANNEL: Lazy<(mpsc::Sender<OpcodeMsg>, Mutex<mpsc::Receiver<Opcod
 });
 
 //Brian Add
-pub static PRINT_CHANNEL: Lazy<(mpsc::Sender<BlockMsg>, Mutex<mpsc::Receiver<BlockMsg>>)> = Lazy::new(|| { 
+pub static PRINT_CHANNEL: Lazy<(mpsc::Sender<Batch>, Mutex<mpsc::Receiver<Batch>>)> = Lazy::new(|| { 
     let (sender, receiver) = mpsc::channel();
     (sender, Mutex::new(receiver))
 });
@@ -29,10 +29,15 @@ pub struct OpcodeMsg {
 pub struct BlockMsg { 
     pub block_num: u128,
     pub op_time_map: HashMap<&'static str, Vec<u128>>,
-    pub write_path: Option<usize>, //may be change to vec index
+    //pub write_path: Option<usize>, //may be change to vec index
 
     //pub op_name_list: Vec<&'a str>,
     //pub run_time_list: Vec<u128>,
+}
+
+pub struct Batch {
+    pub block_msg_vec: Vec<BlockMsg>,
+    pub write_path: Option<usize>,
 }
 
 //pub static mut WRITE_FILE: &File = *File::create("./tmp").unwrap();
@@ -51,6 +56,7 @@ pub struct BlockMsg {
 // });
 //pub static mut WRITE_PATH: &String = &String::new();
 pub static mut WRITE_PATH_VEC: Vec<Mutex<String>> = Vec::new();
+//pub static mut WRITE_PATH: Option<String> = None;
  
 // 使用 lazy_static 来创建一个全局的 HashMap，并用 Mutex 封装
 lazy_static! {
@@ -66,7 +72,7 @@ static CHANNEL: Lazy<(mpsc::Sender<(u8, u128, u128)>, Mutex<mpsc::Receiver<(u8, 
     (sender, Mutex::new(receiver))
 });
 
-pub fn start_channel() -> thread::JoinHandle<()> {
+pub fn start_channel(split: u64) -> thread::JoinHandle<()> {
 
     // unsafe {
     //     let mut file = OpenOptions::new().append(true).open(WRITE_PATH).expect("Can not open file!");
@@ -76,14 +82,20 @@ pub fn start_channel() -> thread::JoinHandle<()> {
         block_num: 0,
         op_time_map: HashMap::new(),
         //write_path: None::<&String>.unwrap(), //fix to WRITE_PATH_VEC.last().unwarp?
-        write_path: None,
+        //write_path: None,
         //op_name_list: Vec::new(),
         //run_time_list: Vec::new(),
     }; //Brian Add
+
+    let mut batch = Batch{
+        block_msg_vec: Vec::<BlockMsg>::new(),
+        write_path: None,
+    };
     
+    let mut counter: u64 = 0;
 
     //Brian Modify
-    let log_handle: thread::JoinHandle<()> = thread::spawn(|| { // 启动一个线程来处理日志
+    let log_handle: thread::JoinHandle<()> = thread::spawn(move || { // 启动一个线程来处理日志
         loop {
             let log_message = { // 锁定接收端，并尝试接收消息
                 let receiver = OP_CHANNEL.1.lock().unwrap();
@@ -92,34 +104,28 @@ pub fn start_channel() -> thread::JoinHandle<()> {
 
             match log_message {
                 Ok(message) => {
-                    if message.op_idx == 0xCC { //0xCC还没对应的opcode，可以拿来做新Block标识，运行到0xCC表示开始运行新块
-                        // PRINT_CHANNEL.0.send(block_msg).unwrap(); //将旧blockmsg实例放入打印管道，等候被打印
-                        // block_msg = BlockMsg{ //新建新块的消息实例
-                        //     block_num: message.run_time,
-                        //     op_time_map: HashMap::new(),
-                        //     write_path: Some(message.writer_path.unwrap()),
-                        //     //op_name_list: Vec::new(),
-                        //     //run_time_list: Vec::new(),
-                        // };
-                        let write_path: Option<usize>;
-                        PRINT_CHANNEL.0.send(block_msg).unwrap();
-                        match message.writer_path {
-                            Some(val) => {
-                                write_path = Some(val);
-                            }
-                            None => {
-                                write_path = None;
-                            }
+                    if message.op_idx == 0xCC {
+                        if block_msg.block_num != 0 {
+                            batch.block_msg_vec.push(block_msg);
+                            counter += 1;
+                        } else {
+                            batch.write_path = message.writer_path;
                         }
-                        block_msg = BlockMsg{ //新建新块的消息实例
+                        if counter == split {
+                            PRINT_CHANNEL.0.send(batch).unwrap();
+                            //eprintln!("Send batch------------------------------------------------------------------------------");
+                            batch = Batch{
+                                block_msg_vec: Vec::<BlockMsg>::new(),
+                                write_path: message.writer_path,
+                            };
+                            counter = 0;
+                        }
+                        block_msg = BlockMsg{
                             block_num: message.run_time,
                             op_time_map: HashMap::new(),
-                            write_path: write_path,
-                            //op_name_list: Vec::new(),
-                            //run_time_list: Vec::new(),
                         };
                     }
-                    else { //往blockMsg实例里填opcode
+                    else {
                         let opcode_str = OpCode::new(message.op_idx).unwrap().as_str();
                         let value = block_msg.op_time_map.get_mut(opcode_str); //区分get_mut 和 get 
                         match value {
@@ -132,14 +138,64 @@ pub fn start_channel() -> thread::JoinHandle<()> {
                                 block_msg.op_time_map.insert(OpCode::new(message.op_idx).unwrap().as_str(), vec);
                             }
                         }
-                        //block_msg.op_name_list.push(OpCode::new(message.op_idx).unwrap().as_str());
-                        //block_msg.run_time_list.push(message.run_time);
                     }
                 }
-                Err(_) => { // 当发送端关闭时，退出循环
-                    break
+                Err(_) => {
+                    break;
                 }
             }
+
+
+            // match log_message {
+            //     Ok(message) => {
+            //         if message.op_idx == 0xCC { //0xCC还没对应的opcode，可以拿来做新Block标识，运行到0xCC表示开始运行新块
+            //             // PRINT_CHANNEL.0.send(block_msg).unwrap(); //将旧blockmsg实例放入打印管道，等候被打印
+            //             // block_msg = BlockMsg{ //新建新块的消息实例
+            //             //     block_num: message.run_time,
+            //             //     op_time_map: HashMap::new(),
+            //             //     write_path: Some(message.writer_path.unwrap()),
+            //             //     //op_name_list: Vec::new(),
+            //             //     //run_time_list: Vec::new(),
+            //             // };
+            //             let write_path: Option<usize>;
+            //             PRINT_CHANNEL.0.send(block_msg).unwrap();
+            //             match message.writer_path {
+            //                 Some(val) => {
+            //                     write_path = Some(val);
+            //                 }
+            //                 None => {
+            //                     write_path = None;
+            //                 }
+            //             }
+            //             block_msg = BlockMsg{ //新建新块的消息实例
+            //                 block_num: message.run_time,
+            //                 op_time_map: HashMap::new(),
+            //                 //write_path: write_path,
+            //                 //op_name_list: Vec::new(),
+            //                 //run_time_list: Vec::new(),
+            //             };
+            //         }
+            //         else { //往blockMsg实例里填opcode
+            //             let opcode_str = OpCode::new(message.op_idx).unwrap().as_str();
+            //             let value = block_msg.op_time_map.get_mut(opcode_str); //区分get_mut 和 get 
+            //             match value {
+            //                 Some(vec) => {
+            //                     vec.push(message.run_time);
+            //                 }
+            //                 None => {
+            //                     let mut vec: Vec<u128> = Vec::new();
+            //                     vec.push(message.run_time);
+            //                     block_msg.op_time_map.insert(OpCode::new(message.op_idx).unwrap().as_str(), vec);
+            //                 }
+            //             }
+            //             //block_msg.op_name_list.push(OpCode::new(message.op_idx).unwrap().as_str());
+            //             //block_msg.run_time_list.push(message.run_time);
+            //         }
+            //     }
+            //     Err(_) => { // 当发送端关闭时，退出循环
+            //         break
+            //     }
+            //}
         }
     });
 
@@ -209,13 +265,14 @@ pub fn print_records(thread_id: u64) -> thread::JoinHandle<()>{
     // }
     let print_handler: thread::JoinHandle<()> = thread::spawn(move || {
         loop {
+            //eprintln!("Thread {} receiving", thread_id);
             let print_message = {
                 let receiver = PRINT_CHANNEL.1.lock().unwrap();
                 receiver.recv()
             };
             match print_message {
                 Ok(message) => {
-                    if /*message.op_name_list.len() > 0*/ message.block_num > 0 { //判断BlockMsg是否为空
+                    if /*message.op_name_list.len() > 0*/ message.write_path != None { //判断BlockMsg是否为空
                         // let path_lock = unsafe { &WRITE_PATH_VEC[message.write_path.unwrap()] };
                         // let path_guard1 = path_lock.lock().unwrap();
                         // let path_guard2 = path_lock.lock().unwrap();
@@ -234,28 +291,31 @@ pub fn print_records(thread_id: u64) -> thread::JoinHandle<()>{
                         //添加判断，如果有其他线程在当前blockMsg相同地址进行输出，则将当前blockMsg再次推入管道
                         match f_lock_res{
                             Ok(f_gard) => { //没有被占用，进行追加写入操作
-                                let mut f = OpenOptions::new().append(true).open(f_gard.clone()).unwrap();
-                                //println!("BlockNumber {}", message.block_num);
-                                f.write(format!("BlockNumber {}\n", message.block_num).as_bytes()).unwrap();
-
-                                for (k, v) in message.op_time_map { //Output
-                                    //print!("{}", k);
-                                    f.write(format!("{}", k).as_bytes()).unwrap();
-                                    for op_time in v {
-                                        //print!(" {}", op_time);
-                                        f.write(format!(" {}", op_time).as_bytes()).unwrap();
+                                //print!("{}", f_gard.clone());
+                                //let mut f = OpenOptions::new().append(false).open(f_gard.clone()).unwrap();
+                                let mut f = File::create(f_gard.clone()).unwrap();
+                                //eprintln!("block_msg_vec len: {}", message.block_msg_vec.len());
+                                for block_msg in message.block_msg_vec {
+                                    f.write(format!("BlockNumber {}\n", block_msg.block_num).as_bytes()).unwrap();
+                                    for (k, v) in block_msg.op_time_map { //Output
+                                        //print!("{}", k);
+                                        f.write(format!("{}", k).as_bytes()).unwrap();
+                                        for op_time in v {
+                                            //print!(" {}", op_time);
+                                            f.write(format!(" {}", op_time).as_bytes()).unwrap();
+                                        }
+                                        //print!("\n");
+                                        f.write(String::from("\n").as_bytes()).unwrap();
                                     }
-                                    //print!("\n");
-                                    f.write(String::from("\n").as_bytes()).unwrap();
-                                }
-                                f.flush().unwrap();
-                                unsafe { //标记提交打印次数
-                                    *COUNT.write().unwrap() += 1;
-                                    eprintln!("Thread {}. Done {}", thread_id, *COUNT.read().unwrap());
+                                    f.flush().unwrap();
+                                    unsafe { //标记提交打印次数
+                                        *COUNT.write().unwrap() += 1;
+                                        eprintln!("Thread {}. Done {}", thread_id, *COUNT.read().unwrap());
+                                    }
                                 }
                             }
                             Err(_) => { //冲突，推入管道
-                                PRINT_CHANNEL.0.send(message).unwrap();
+                                
                             }
                         }
                     }
@@ -270,10 +330,10 @@ pub fn print_records(thread_id: u64) -> thread::JoinHandle<()>{
 }
 
 //Brian Add
-pub fn wait(block_cnt: u64) { //等待执行完毕
+pub fn wait(block_cnt: u64, split: u64) { //等待执行完毕
     unsafe {
         loop {
-            if *COUNT.read().unwrap() == block_cnt - 1 { //已经执行完倒数第二个
+            if *COUNT.read().unwrap() == block_cnt - split { //已经执行完倒数第二个
                 OP_CHANNEL.0.send(OpcodeMsg{op_idx: 0xCC, run_time: 0, writer_path: None}).unwrap(); //发个信号，将最后一个blockMsg放进管道
                 loop {
                     if *COUNT.read().unwrap() == block_cnt {
@@ -282,6 +342,7 @@ pub fn wait(block_cnt: u64) { //等待执行完毕
                     }
                 }
             }
+            thread::sleep(core::time::Duration::from_secs(1));
         }
     } 
 }
